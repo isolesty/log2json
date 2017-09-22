3b5a4861607207e26fa66fab3fb27ba5f97ea267
isoletsy <xielingyun@linuxdeepin.com>
2017-09-22 09:55:58 +0800

    Fix dsc filename check
    dde*.dsc and dde_*.dsc matched different files

--- newrpa.py
+++ newrpa.py
@@ -132,9 +132,9 @@ def gen_source(itemlist):
             if item['arch'] == 'source':
                 # get information in *.dsc
                 section = subprocess.check_output(["sed -n '/Package-List:/{n;p}' " +
-                                                   source + "*.dsc | awk '{print $3}'"], shell=True).strip().decode('utf-8')
+                                                   source + "_*.dsc | awk '{print $3}'"], shell=True).strip().decode('utf-8')
                 priority = subprocess.check_output(["sed -n '/Package-List:/{n;p}' " +
-                                                    source + "*.dsc | awk '{print $4}'"], shell=True).strip().decode('utf-8')
+                                                    source + "_*.dsc | awk '{print $4}'"], shell=True).strip().decode('utf-8')
                 newsource._set_details(section, priority, component)
             else:
                 # one deb file is enough
